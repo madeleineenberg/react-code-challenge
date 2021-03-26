9ca93641ff52f5d0b55201ec7e104e83e29247e7
madeleineenberg <madeleine_enberg@hotmail.com>
2021-03-26 23:23:48 +0100

    image slider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+import heroimg from '../images/hero.png';
 
 export default function Hero() {
   return (
-    <header className='hero'>
-      <div className='hero__content'>
-        <h1>Welcome to Happyweb</h1>
-        <Link to='/about' className='hero__content-button'>
-          See more
-        </Link>
-      </div>
-    </header>
+    <div className='header'>
+      <Carousel autoPlay={false} infiniteLoop={true}>
+        <div>
+          <img src={heroimg} alt='hero' />
+          <div className='header-content'>
+            <h1>Welcome to Happyweb</h1>
+            <Link to='/about' className='header-content-button'>
+              See more
+            </Link>
+          </div>
+        </div>
+        <div>
+          <img src={heroimg} alt='hero' />
+        </div>
+        <div>
+          <img src={heroimg} alt='hero' />
+        </div>
+      </Carousel>
+    </div>
   );
 }
