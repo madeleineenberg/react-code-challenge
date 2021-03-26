fadb365e6449746afa3324a3ed870f1fc5d1e999
madeleineenberg <madeleine_enberg@hotmail.com>
2021-03-26 11:54:37 +0100

    font

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom';
 import './App.scss';
+import Navigation from './components/Navigation';
 import About from './pages/About';
 import Career from './pages/Career';
 import Startpage from './pages/Startpage';
@@ -9,9 +10,20 @@ function App() {
   return (
     <Switch>
       
-      <Route path="/about" component={About}/>
-      <Route path="/career" component={Career}/>
-      <Route path="/" component={Startpage}/>
+      <Route path="/about">
+      <Navigation/>
+      <About/>
+      </Route>
+
+      <Route path="/career">
+        <Navigation/>
+        <Career/>
+        </Route>
+
+      <Route path="/">
+        <Navigation/>
+        <Startpage/>
+        </Route>
      
     </Switch>
   );
