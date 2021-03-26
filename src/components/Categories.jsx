958c002df7927b482369892e2698ad9d8479137e
madeleineenberg <madeleine_enberg@hotmail.com>
2021-03-26 12:49:40 +0100

    responsive category section

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -6,23 +6,29 @@ import ngo from '../images/NGO.png';
 export default function Categories() {
   return (
     <section className='category-section'>
-      <div className='category-section__item item-1'>
+      <div className='category-section__item'>
         <div className='category-section__item-content'>
-          <img src={giftcard} alt='gift card' />
+          <div className='img-wrapper'>
+            <img src={giftcard} alt='gift card' />
+          </div>
           <h2>Gift Card</h2>
         </div>
       </div>
 
-      <div className='category-section__item item-2'>
+      <div className='category-section__item'>
         <div className='category-section__item-content'>
-          <img src={happycard} alt='happy card' />
+          <div className='img-wrapper'>
+            <img src={happycard} alt='happy card' />
+          </div>
           <h2>Happy Card</h2>
         </div>
       </div>
 
-      <div className='category-section__item item-3'>
+      <div className='category-section__item'>
         <div className='category-section__item-content'>
-          <img src={ngo} alt='NGO' />
+          <div className='img-wrapper'>
+            <img src={ngo} alt='NGO' />
+          </div>
           <h2>Happi Offers</h2>
         </div>
       </div>
